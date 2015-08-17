3d69a95ea506a8192843ba91b911f6b24ca33232
Will Craft <wcraft1204@gmail.com>
2015-08-17 16:43:48 -0700

    I moved files to better locations

--- tools-and-scripts/pull-california-pipeline-operators/pipeline_csvs/pipeline-incident-details.py
+++ tools-and-scripts/pull-california-pipeline-operators/pipeline_csvs/pipeline-incident-details.py
@@ -24,7 +24,7 @@ def _init_():
     """
     kicks off the whole process
     """
-    get_ca_op_incident_details()
+    get_all_incident_details()
     
 
 def get_all_incident_details():
@@ -53,29 +53,6 @@ def get_all_incident_details():
             except:
                 pass
 
-def get_ca_op_incident_details(): 
-    #start from main operator list, go into each to see if it's in CA. If in CA, jump to incident tab html and grab rows that are only present in CA 
-    main_page = open_page_and_get_soup("http://primis.phmsa.dot.gov/comm/reports/operator/OperatorListNoJS.html")
-    op_ids = main_page.find('tbody').find_all("tr")
-    for id in op_ids:
-         config["operator_ids"].append(id.find('td').text.strip())
-    #constructs a list of pipeline incident details urls
-    url_prefix = "http://primis.phmsa.dot.gov/comm/reports/operator/OperatorIM_opid_"
-    url_suffix = ".html?nocache=%s" % (randint(1000, 1999))
-    list_operator_incident_urls = []
-    for operator in config["operator_ids"]:
-        url = build_url(url_prefix, operator, url_suffix)
-        raw_html = open_page_and_get_soup(url)
-        if raw_html.find(text = "CA"):
-            find_incidents_list_items = get_list_items(raw_html, "Incidents", -1)
-            incident_div_tag  = find_incidents_list_items["url_stub"].replace("#","")
-            try:
-                write_to_ca_csv(raw_html, incident_div_tag)
-            except:
-                pass
-            
-            
-
 
 def write_to_csv(html,div_tag):
     list_of_rows = []
@@ -91,20 +68,6 @@ def write_to_csv(html,div_tag):
     writer = csv.writer(csv_file)
     writer.writerows(list_of_rows)
 
-# I know I shouldn't just copy the function with one small change but I will clean it up later
-def write_to_ca_csv(html,div_tag):
-    list_of_rows = []
-    list_tr = html.find("div", {"id": div_tag }).find_all("tr")
-    op_name  = html.find("h4").text.lower().replace(" ","_")
-    column_names = map(lambda x: x.text.encode("utf8").strip().lower().replace(" ", "_").replace("\xc2\xa0",""), list_tr[0].find_all("th"))
-    for item in list_tr[1:-1]:
-        if item.find_all("td")[3].text.strip() == "CA":
-            rows.append(column_names)
-            rows.append(map(lambda x: x.text.encode("utf8").strip().replace(",",""), item.find_all('td')[0:-1]))
-            
-    csv_file = open(op_name+".csv", "wb")
-    writer = csv.writer(csv_file)
-    writer.writerows(list_of_rows)
 
 
 def get_list_items(html, id, position):
